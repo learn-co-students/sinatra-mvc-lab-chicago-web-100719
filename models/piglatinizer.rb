4782e5c80a8721c83c6dc820aaffb1d2c98d6ad8
Janae Hall <janaeelisa@gmail.com>
2019-10-29 18:19:43 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,36 @@
+
+class PigLatinizer
+    def piglatinize(input)
+        if input.split(" ").length == 1
+            input = self.piglatinize_word(input)
+        else
+            input = self.piglatinize_sentence(input)
+        end
+    end
+
+    def consonant?(char)
+        !char.match(/[aAeEiIoOuU]/)
+    end
+
+    def piglatinize_word(word)
+        if !consonant?(word[0])
+            word += "w"
+        elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+            word = word.slice(3..-1) + word.slice(0..2)
+        elsif consonant?(word[0]) && consonant?(word[1])
+            word = word.slice(2..-1) + word.slice(0..1)
+        elsif consonant?(word[0])
+            word = word.slice(1..-1) + word.slice(0)
+        end
+        word + "ay"
+    end
+
+    def piglatinize_sentence(sentence)
+        array = sentence.split(" ")
+        array.map{|word| self.piglatinize_word(word)}.join(" ")
+    end
+
+    
+
+
+end
